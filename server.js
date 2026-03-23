2bcf5c0a7a062895ad392ef5cca6ff45b56c5376
ayoubcr5 <36039958+ayoubcr5@users.noreply.github.com>
2026-03-23 16:22:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ cors_proxy.createServer({
   removeHeaders: [
     'cookie',
     'cookie2',
+     'host',
+  'connection',
     // Strip Heroku-specific headers
     'x-request-start',
     'x-request-id',
